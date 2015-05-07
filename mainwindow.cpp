6b31350d127fa6aaf53cce0fc33ce036292794e3
Alex Ferguson <asf04a@gmail.com>
2015-05-07 13:56:02 -0500

    Added slot for Install button, implemented and tested code for first two printers.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,15 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_installButton_clicked()
+{
+    if(ui->ADM108_1C->isChecked())
+    {
+        system("lpadmin -p ADM108_1C -L \"ADM108_1C\" -E -v lpd://150.252.11.162 -P /Library/Printers/PPDs/Contents/Resources/CNPZUIRA4225ZU.ppd.gz -o printer-is-shared=false -o CNColorMode=Mono -o CNDuplex=None");
+    }
+    if(ui->ADM204_1C->isChecked())
+    {
+        system("lpadmin -p ADM204_1C -L \"ADM204_1C\" -E -v lpd://150.252.12.34 -P /Library/Printers/PPDs/Contents/Resources/CNPZUIRAC2225ZU.ppd.gz -o printer-is-shared=false -o CNColorMode=Mono -o CNDuplex=None -o CNStaple=True");
+    }
+}
